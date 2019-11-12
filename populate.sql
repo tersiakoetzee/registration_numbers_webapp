78805f747dd75e9e0194c599c899b94ceb216a13
Tersia Koetzee <tersiakoetzee@gmail.com>
2019-11-12 17:45:45 +0200

    travis file fixed

---
+++ populate.sql
@@ -0,0 +1,3 @@
+insert into town (town, tag) values ('Malmesbury', 'CK');
+insert into town (town, tag) values ('Cape Town', 'CA');
+insert into town (town, tag) values ('Bellville', 'CY');
--- tables.sql
+++ tables.sql
@@ -1,11 +1,14 @@
 create table town(
-id serial not null primary key,
-town text not null,
-tag text not null 
+    id serial not null primary key,
+    town text not null,
+    tag text not null 
 );
+
 create table regs(
-id serial not null primary key,
-regnumber text not null,
-town_id int not null,
-foreign key (town_id) references town(id)
+    id serial not null primary key,
+    regnumber text not null,
+    town_id int not null,
+    foreign key (town_id) references town(id)
 );
+
+
