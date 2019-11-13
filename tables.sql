c2b1efefd7912dc6eb5d44fd2138cbc04c4d5fe6
Tersia Koetzee <tersiakoetzee@gmail.com>
2019-11-13 06:47:58 +0200

    database for heroku

--- tables.sql
+++ tables.sql
@@ -4,11 +4,6 @@ create table town(
     tag text not null 
 );
 
-create table regs(
-    id serial not null primary key,
-    regnumber text not null,
-    town_id int not null,
-    foreign key (town_id) references town(id)
-);
+
 
 
